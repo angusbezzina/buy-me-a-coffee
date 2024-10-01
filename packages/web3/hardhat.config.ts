69a8e2e4697e69be5d24abada7b9488b1d6b4b8d
Angus <37071175+angusbezzina@users.noreply.github.com>
2024-09-30 22:55:33 -0500

    feat: refactor repo with npm, typescript and viem

---
+++ packages/web3/hardhat.config.ts
@@ -0,0 +1,9 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-viem";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.27",
+};
+
+export default config;
