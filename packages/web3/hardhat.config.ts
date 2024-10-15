4d1a1c5f60f13a6a33ff35f2a1f4cb18d750309e
Angus <angusbezzina@gmail.com>
2024-10-14 20:38:30 -0500

    feat: web3 tweaks

--- packages/web3/hardhat.config.ts
+++ packages/web3/hardhat.config.ts
@@ -1,9 +1,25 @@
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-viem";
+import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
+
+dotenv.config();
+
+const { SEPOLIA_PRIVATE_KEY = "", SEPOLIA_URL = "" } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_URL,
+      accounts: [`0x${SEPOLIA_PRIVATE_KEY}`],
+      chainId: 11155111, // Sepolia Chain ID
+    },
+    // mainnet: {
+    //   url: MAINNET_RPC_URL,
+    //   accounts: [MAINNET_PRIVATE_KEY],
+    // },
+  },
 };
 
 export default config;
